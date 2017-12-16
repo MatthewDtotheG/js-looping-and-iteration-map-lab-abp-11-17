7769e7e6871b8f8603eb5572558d0c1e2332046d
Matthew Garrett <mdgarrett1984@gmail.com>
2017-12-16 16:49:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-// Code your solution in this file.
-function lowerCaseDrivers(array) {
-  return array.map(function(e) {
-    return e.toLowerCase();
+function lowerCaseDrivers (list) {
+  return list.map(function (driver) {
+    return driver.toLowerCase();
   });
 }
 
-function nameToAttributes(names) {
-  // names = ['Bobby Smith', 'Sammy Watkins']
-  return names.map(function(name) {
-    // name 'Bobby Smith'
-    let fName = name.split(' ')[0];
-    let lName = name.split(' ')[1];
-    return { firstName: fName, lastName: lName };
-    //
+function nameToAttributes (list) {
+  return list.map(function (driver) {
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+
+    return { firstName: driverFirst, lastName: driverLast };
   });
-  // [{firstName: "Bobby", lastName: ""}]
 }
 
 function attributesToPhrase(drivers) {
-  return drivers.map(function(driver) {});
+  return drivers.map(function (driver) {
     return `${driver.name} is from ${driver.hometown}`;
+  });
 }
